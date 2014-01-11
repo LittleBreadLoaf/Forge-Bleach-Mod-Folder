a3ec43097fb9152c4aace83bdcf4a6d41c7409d8
Dawars <dawars00@gmail.com>
2014-01-11 21:27:07 +0100

    Shinigami zanpakuto fix

--- littlebreadloaf/entities/renders/RenderShinigami.java
+++ littlebreadloaf/entities/renders/RenderShinigami.java
@@ -43,51 +43,14 @@ public class RenderShinigami extends RenderBiped
 		super(par1ModelBiped, par2);
 		model = ((ModelBiped) mainModel);
 	}
-	protected void renderEquippedItems(EntityLivingBase entity, float par2)
+	
+	@Override
+	protected void func_130005_c(EntityLiving par1EntityLiving, float par2)
     {
         float f1 = 1.0F;
         GL11.glColor3f(f1, f1, f1);
-        super.renderEquippedItems(entity, par2);
-        ItemStack itemstack = entity.getHeldItem();
-        ItemStack itemstack1 = ((EntityLiving) entity).func_130225_q(3);
+        ItemStack itemstack = par1EntityLiving.getHeldItem();
         float f2;
-
-        if (itemstack1 != null)
-        {
-            GL11.glPushMatrix();
-            this.modelBipedMain.bipedHead.postRender(0.0625F);
-
-            IItemRenderer customRenderer = MinecraftForgeClient.getItemRenderer(itemstack1, EQUIPPED);
-            boolean is3D = (customRenderer != null && customRenderer.shouldUseRenderHelper(EQUIPPED, itemstack1, BLOCK_3D));
-
-            if (itemstack1.getItem() instanceof ItemBlock)
-            {
-                if (is3D || RenderBlocks.renderItemIn3d(Block.blocksList[itemstack1.itemID].getRenderType()))
-                {
-                    f2 = 0.625F;
-                    GL11.glTranslatef(0.0F, -0.25F, 0.0F);
-                    GL11.glRotatef(90.0F, 0.0F, 1.0F, 0.0F);
-                    GL11.glScalef(f2, -f2, -f2);
-                }
-
-                this.renderManager.itemRenderer.renderItem(entity, itemstack1, 0);
-            }
-            else if (itemstack1.getItem().itemID == Item.skull.itemID)
-            {
-                f2 = 1.0625F;
-                GL11.glScalef(f2, -f2, -f2);
-                String s = "";
-
-                if (itemstack1.hasTagCompound() && itemstack1.getTagCompound().hasKey("SkullOwner"))
-                {
-                    s = itemstack1.getTagCompound().getString("SkullOwner");
-                }
-
-                TileEntitySkullRenderer.skullRenderer.func_82393_a(-0.5F, 0.0F, -0.5F, 1, 180.0F, itemstack1.getItemDamage(), s);
-            }
-
-            GL11.glPopMatrix();
-        }
 
         if (itemstack != null)
         {
@@ -102,63 +65,26 @@ public class RenderShinigami extends RenderBiped
             }
 
             this.modelBipedMain.bipedRightArm.postRender(0.0625F);
-            GL11.glTranslatef(-0.0625F, 0.4375F, 0.0625F);
+//            GL11.glTranslatef(-0.0625F, 0.4375F, 0.0625F);
 
             IItemRenderer customRenderer = MinecraftForgeClient.getItemRenderer(itemstack, EQUIPPED);
             boolean is3D = (customRenderer != null && customRenderer.shouldUseRenderHelper(EQUIPPED, itemstack, BLOCK_3D));
 
-            if (itemstack.getItem() instanceof ItemBlock && (is3D || RenderBlocks.renderItemIn3d(Block.blocksList[itemstack.itemID].getRenderType())))
+            if (itemstack.itemID == Items.zanpakuto.itemID)
             {
-                f2 = 0.5F;
-                GL11.glTranslatef(0.0F, 0.1875F, -0.3125F);
-                f2 *= 0.75F;
-                GL11.glRotatef(20.0F, 1.0F, 0.0F, 0.0F);
-                GL11.glRotatef(45.0F, 0.0F, 1.0F, 0.0F);
-                GL11.glScalef(-f2, -f2, f2);
-            }
-            else if (itemstack.itemID == Items.zanpakuto.itemID)
-            {//TODO: fix rotation
-                f2 = 0.625F;
-                GL11.glTranslatef(0.0F, 0.125F, 0.3125F);
-                GL11.glRotatef(-20.0F, 0.0F, 1.0F, 0.0F);
-                GL11.glScalef(f2, -f2, f2);
-                GL11.glRotatef(-100.0F, 1.0F, 0.0F, 0.0F);
-                GL11.glRotatef(45.0F, 0.0F, 1.0F, 0.0F);
-            }
-            else if (Item.itemsList[itemstack.itemID].isFull3D())
-            {
-                f2 = 0.625F;
-
-                if (Item.itemsList[itemstack.itemID].shouldRotateAroundWhenRendering())
-                {
-                    GL11.glRotatef(180.0F, 0.0F, 0.0F, 1.0F);
-                    GL11.glTranslatef(0.0F, -0.125F, 0.0F);
-                }
-
-                this.func_82422_c();
-                GL11.glScalef(f2, -f2, f2);
-                GL11.glRotatef(-100.0F, 1.0F, 0.0F, 0.0F);
-                GL11.glRotatef(45.0F, 0.0F, 1.0F, 0.0F);
-            }
-            else
-            {
-                f2 = 0.375F;
-                GL11.glTranslatef(0.25F, 0.1875F, -0.1875F);
+                f2 = 0.4F;
                 GL11.glScalef(f2, f2, f2);
-                GL11.glRotatef(60.0F, 0.0F, 0.0F, 1.0F);
-                GL11.glRotatef(-90.0F, 1.0F, 0.0F, 0.0F);
-                GL11.glRotatef(20.0F, 0.0F, 0.0F, 1.0F);
+
+                GL11.glTranslatef(0.13F, -0.15F, -0.75F);
+                GL11.glRotatef(45.0F, 0.0F, 0.0F, 1.0F);
+            	GL11.glRotatef(-45.0F, 0.0F, 1.0F, 0.0F);
+                GL11.glRotatef(-55F, 1.0F, 0.0F, 0.0F);
+
+                customRenderer.renderItem(EQUIPPED, itemstack);
             }
 
-            this.renderManager.itemRenderer.renderItem(entity, itemstack, 0);
+//            this.renderManager.itemRenderer.renderItem(par1EntityLiving, itemstack, 0);
 
-            if (itemstack.getItem().requiresMultipleRenderPasses())
-            {
-                for (int x = 1; x < itemstack.getItem().getRenderPasses(itemstack.getItemDamage()); x++)
-                {
-                    this.renderManager.itemRenderer.renderItem(entity, itemstack, x);
-                }
-            }
 
             GL11.glPopMatrix();
         }
